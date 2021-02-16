cf6b1779fcb5288e8d6b3c7da6ddc6f6b2598be7
plu0907 <68453161+plu0907@users.noreply.github.com>
2021-02-15 21:06:18 -0500

    Overhaul

--- js/script.js
+++
@@ -1,9 +0,0 @@
-function changeText(){ 
-	var current_val = document.getElementById('my_description').innerText 
-	if (current_val == "Teacher"){ 
-		document.getElementById('my_description').innerHTML = "Student" 
-	}
-	else{ 
-		document.getElementById('my_description').innerHTML = "Teacher" 
-	} 
-}
